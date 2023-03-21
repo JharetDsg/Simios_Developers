360d8999910b37ed2fe4291c6cc68ae9bd5839cb
jhare <jharet696@gmail.com>
2023-03-20 21:53:00 -0500

    KJPL

---
+++ KJPL/KJPL/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Request: Cuando requiero parametros en la vista desde la plantilla (Forms)
+#           implementa por defecto los token CSRF - Cross Site Scripting
+# Response: Cuando enviamos datos de la vista a la plantilla
+def inicio(request):
+    return render(request, "Contacto.html")
